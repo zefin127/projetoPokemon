31c786d94122444c41bc90b944838aeffac1d574
Fernando <marcoscraft47@gmail.com>
2024-06-12 11:47:47 -0300

    Subindo Arquivos

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+       'sans':['Poppins', 'sans-serif']
+    },
+    extend:{}
+    
+    },
+  plugins: [],
+
+}
+
